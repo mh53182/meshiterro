0dda96d4168dd805b56cf6d2a40c64ee71983bf2
mh53182 <marie.i.077145@gmail.com>
2023-10-11 07:19:47 +0000

    [Add]PostImageに対する詳細画面、削除機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
   # 引数(resource)にはログインしたユーザーのインスタンスが格納されている
-    about_path
+    post_images_path
   end
 
   def after_sign_out_path_for(resource)
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -6,10 +6,10 @@ class PostImage < ApplicationRecord
 
   def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/image/no_image.jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
       image.atacch(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    
+    image
   end
 
 end
